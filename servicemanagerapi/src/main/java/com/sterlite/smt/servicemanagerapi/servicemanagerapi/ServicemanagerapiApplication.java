29a42fc6178ed21e7a895e19b6db89b56239bcb0
Deep patel <sherathiyadeep2469@gmail.com>
2022-02-22 15:22:03 +0530

    added repo class and changed other files

--- servicemanagerapi/src/main/java/com/sterlite/smt/servicemanagerapi/servicemanagerapi/ServicemanagerapiApplication.java
+++ servicemanagerapi/src/main/java/com/sterlite/smt/servicemanagerapi/servicemanagerapi/ServicemanagerapiApplication.java
@@ -1,13 +1,28 @@
 package com.sterlite.smt.servicemanagerapi.servicemanagerapi;
 
+import com.sterlite.smt.servicemanagerapi.servicemanagerapi.enumeration.Status;
+import com.sterlite.smt.servicemanagerapi.servicemanagerapi.model.Server;
+import com.sterlite.smt.servicemanagerapi.servicemanagerapi.repositories.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServicemanagerapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicemanagerapiApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo){
+		return args -> {
+			serverRepo.save(new Server(null, "10.123.10.12", "Deep Linux", "32 GB", "Working Env",
+					"http://localhost:8088/server/image/server1.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "10.189.23.89", "Windows", "64 GB", "Gaming Env",
+					"http://localhost:8088/server/image/server2.png", Status.SERVER_UP));
+		};
 	}
 
 }
---
+++ servicemanagerapi/src/main/java/com/sterlite/smt/servicemanagerapi/servicemanagerapi/resources/ServerResource.java
@@ -0,0 +1,106 @@
+package com.sterlite.smt.servicemanagerapi.servicemanagerapi.resources;
+/*
+ * author: deepkumar.sherathiya
+ * version: 1.0
+ * date: 22/02/2022
+ */
+
+import com.sterlite.smt.servicemanagerapi.servicemanagerapi.enumeration.Status;
+import com.sterlite.smt.servicemanagerapi.servicemanagerapi.model.Response;
+import com.sterlite.smt.servicemanagerapi.servicemanagerapi.model.Server;
+import com.sterlite.smt.servicemanagerapi.servicemanagerapi.services.implementation.ServerServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
+
+@RestController
+@RequestMapping("/server")
+@RequiredArgsConstructor
+public class ServerResource {
+    private final ServerServiceImpl serverService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Response> getServers(){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.list(20)))
+                        .message("Server retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+
+        Server server = serverService.ping(ipAddress);
+
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", server))
+                        .message(server.getStatus() == Status.SERVER_UP ? "Ping Success" : "Ping Failed")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.create(server)))
+                        .message("Server Created successfully")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.get(id)))
+                        .message("Server retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("deleted", serverService.delete(id)))
+                        .message("Server deleted successfully")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping(path = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
+    public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(
+                "/home/sterlite/Projects/SMT/ServerImages/" + fileName));
+    }
+}

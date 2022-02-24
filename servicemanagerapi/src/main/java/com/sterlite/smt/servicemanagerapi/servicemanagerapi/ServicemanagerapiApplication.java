504dc8db4a2dc7d65a947c7583f39e5ece7120f7
Deep patel <sherathiyadeep2469@gmail.com>
2022-02-24 17:21:44 +0530

    change in application class in backness and added service in angular

--- servicemanagerapi/src/main/java/com/sterlite/smt/servicemanagerapi/servicemanagerapi/ServicemanagerapiApplication.java
+++ servicemanagerapi/src/main/java/com/sterlite/smt/servicemanagerapi/servicemanagerapi/ServicemanagerapiApplication.java
@@ -8,6 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.sterlite.smt.servicemanagerapi.servicemanagerapi.repositories"})
@@ -25,6 +30,23 @@ public class ServicemanagerapiApplication {
 			serverRepo.save(new Server(null, "10.189.23.89", "Windows", "64 GB", "Gaming Env",
 					"http://localhost:8088/server/image/server2.png", Status.SERVER_UP));
 		};
+	}
+
+	@Bean
+	public CorsFilter corsFilter(){
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token",
+				"Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials",
+				"Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 
 }
--- servicemanagergui/src/app/interface/app-state.ts
+++ servicemanagergui/src/app/interface/app-state.ts
@@ -1,7 +1,7 @@
 import { DataState } from "../enum/data-state.enum";
 
 export interface AppState<T> {
-    dateState: DataState;
+    dataState: DataState;
     appData?: T;
     error?: string;
 }
